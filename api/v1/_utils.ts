f6137e3248216c5cc02e306723c5d4152be14840
Yari55 <102930715+Yari55@users.noreply.github.com>
2023-12-22 22:39:52 +0800

    Update _utils.ts

--- api/v1/_utils.ts
+++ api/v1/_utils.ts
@@ -4,7 +4,7 @@ import { Settings } from './_types';
 const BASE_URL = 'https://serpapi.com/search';
 
 const fetchResult = async (args: { query: string }, settings: Settings): Promise<Result> => {
-  const apiKey = settings.SERPAPI_API_KEY;
+  const apiKey = 'Ee67b4c94393bd6d29892759a35b0b1e3834dd22baa7bf6fb14312d27fd61115';
 
   const { default: querystring } = await import('query-string');
 
